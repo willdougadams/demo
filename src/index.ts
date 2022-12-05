99425c042cf810d653722c394412a946eec8c653
Will Adams <will@Wills-MacBook-Pro-2.local>
2022-12-04 21:55:26 -0800

    port and procfile

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.get("/", (req: Request, res: Response, next: NextFunction): void => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
