ba1b2b20644a2a2d462aa9a8835bef7105dfc7a4
Will Adams <will@Wills-MacBook-Pro-2.local>
2022-12-10 16:42:15 -0800

    navbar, linting

--- client/App.tsx
+++ client/App.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Home } from './home/Home'
+import { NavBar } from "./navBar/NavBar";
+
+
 const App: React.FC = () => {
-  return <h1>TypeScript is awesome</h1>;
+  return (
+    <NavBar />
+    // <Router>
+    //   <div>
+    //     <h2>Welcome to React Router Tutorial</h2>
+    //     <nav>
+    //     <div>
+    //       <Link to={'/'}> Home </Link>
+    //       <Link to={'/contact'}>Contact</Link>
+    //       <Link to={'/about'}>About</Link>
+    //     </div>
+    //     </nav>
+    //     <hr />
+    //     <Routes>
+    //         <Route path='/' element={<Home />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
+  )
 };
 
 export default App;
--- client/index.tsx
+++ client/index.tsx
@@ -1,10 +1,44 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 
-ReactDOM.render(
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
+// const hotdogStandTheme = createTheme({
+//   palette: {
+//     primary: {
+//       light: '#ff5935',
+//       main: '#f90000',
+//       dark: '#bd0000',
+//       contrastText: '#000',
+//     },
+//     secondary: {
+//       light: '#ffff99',
+//       main: '#f5d768',
+//       dark: '#c0a638',
+//       contrastText: '#000',
+//     },
+//   },
+// });
+
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("app")
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
